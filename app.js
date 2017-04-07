2a6d4353926e1f5fe5a5cdbbd225394bdb5d3443
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2017-04-06 18:36:47 -0600

    Create function to update

--- app.js
+++ app.js
@@ -13,19 +13,44 @@ var url = "mongodb://localhost:27017/restaurant_db";
 // 	});
 
 
-	mongo.connect(url, function(err, db){
-	var collection = db.collection('restaurants');
+	// mongo.connect(url, function(err, db){
+	// var collection = db.collection('restaurants');
 
-	var newName = prompt('design your own restaurant, what will you call it?');
-
-
-	var newRest = (newName);
-
-	collection.insert({name:newRest});
+	// var newName = prompt('design your own restaurant, what will you call it?');
 
 
+	// var newRest = (newName);
+
+	// collection.insert({name:newRest});
+
+
+
+// });
+
+mongo.connect(url, function(err, db){
+var collection = db.collection('restaurants');
+
+var updateRest = prompt('Which restaurant do you want to update?');
+
+var newName = prompt('What name will replace the old name?');
+var newStreet = prompt('What will the new street be?');
+var newZip = prompt('What will be the new zipcode?');
+var newYelp = prompt('What will be the new yelp rating?');
+
+
+
+collection.update(
+
+	{name: updateRest}, 
+
+	{$set: {name: newName,
+		address:{
+			street: newStreet,
+			zipcode: newZip
+		},
+		yelp: newYelp,
+	
+}});
 
 });
-
-
 
